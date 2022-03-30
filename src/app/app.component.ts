4b6c55001ccf3c61cde40b67f72673068c82ab45
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-03-30 20:09:53 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  
   title = 'memeAr';
+  imagePath = '';
+  randomImgUrl='https://picsum.photos/536/354';
+  imageLogo= 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1a816f98-6c2a-4d39-b48b-cc21736d55f3/d4utay6-4fdfad3f-96ae-4746-b63f-caa469558cb3.png/v1/fill/w_900,h_810,strp/meme_png_by_yazsexydilemma_d4utay6-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODEwIiwicGF0aCI6IlwvZlwvMWE4MTZmOTgtNmMyYS00ZDM5LWI0OGItY2MyMTczNmQ1NWYzXC9kNHV0YXk2LTRmZGZhZDNmLTk2YWUtNDc0Ni1iNjNmLWNhYTQ2OTU1OGNiMy5wbmciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.hMYgKmN2Pnd8WpbNN4JhCnGs6QNQB_-0ofKbW98h1rs';
+
+  getRandomImgUrl() {
+    return this.randomImgUrl + '?cache=' + new Date().getTime()
+  }
+
+  searchMeme(){
+     this.imagePath = this.getRandomImgUrl();
+  }
 }
